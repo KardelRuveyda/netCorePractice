41c6a5d4924eb2b3a51ac23b1f7a674bd9b0bc69
Kardel Rüveyda Çetin <ruveydakardelcetin@gmail.com>
2020-05-28 00:39:14 +0300

    Add project files.

---
+++ K01.BehaviorofObjects/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace K01.BehaviorofObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = string.Empty;
+            List<string> isimler = new List<String>();
+
+            Musteri musteri = new Musteri();
+            List<Musteri> musteriler = new List<Musteri>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                ad = FakeData.NameData.GetFirstName();
+                isimler.Add(ad);
+            }
+            Console.WriteLine("Primitive");
+
+
+            foreach (var item in isimler)
+            {
+                 Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                musteri.Ad   = FakeData.NameData.GetFirstName();
+                musteriler.Add(musteri);
+            }
+            Console.WriteLine("Refere");
+
+
+            foreach (var item in musteriler)
+            {
+                Console.WriteLine(item.Ad);
+            }
+
+
+
+
+        }
+    }
+}
+
+class Musteri
+{
+    public string Ad { get; set; }
+}
+
