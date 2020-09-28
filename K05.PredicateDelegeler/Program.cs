3451a1798e140bcd1627dca551cb55d3bf053b0c
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-28 19:34:45 +0300

    Predicate Delegate

---
+++ K05.PredicateDelegeler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace K05.PredicateDelegeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "kardel", "rüveyda", "cetin" };
+            Predicate<string> predicate = new Predicate<string>(KardeliBul);
+            list.Find(predicate);
+
+            Console.WriteLine(list.Find(KardeliBul));
+
+        }
+
+        private static bool KardeliBul(string obj)
+        {
+            return obj.ToLower() == "kardel";
+        }
+    }
+}
