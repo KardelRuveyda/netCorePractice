f431c526ad290f859f51df91dc7122263a91e7ca
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-28 19:41:58 +0300

    FuncDelegete

---
+++ K06.FuncDelege/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace K06.FuncDelege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "kardel", "rüveyda", "cetin" };
+
+            Func<string, bool> func = new Func<string, bool>(KardeliBul);
+
+            foreach (var item in list.Where(func))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static bool KardeliBul(string arg)
+        {
+            return arg == "kardel" || arg == "rüveyda";
+        }
+    }
+}
