96202d42cee95185cd0840c981cf6b99ed7e9e23
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-28 19:28:19 +0300

    Action Deleges

---
+++ K04.ActionDeleges/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace K04.ActionDeleges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sayilar = new List<int> { 1, 2, 3, 4, 5, 6 };
+            Action<int> action = new Action<int>(SayiGetir);
+
+            sayilar.ForEach(action);
+        }
+
+        private static void SayiGetir(int obj) => Console.WriteLine(obj.ToString());
+    }
+}
