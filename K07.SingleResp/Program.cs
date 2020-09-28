0bd59f0ba1bb50da8e82df5d48435e70bb1135f6
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-29 00:29:32 +0300

    Single Respons

---
+++ K07.SingleResp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace K07.SingleResp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Musteri
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="Ad alanı boş geçilemez. ")]
+        public string Ad { get; set; }
+        public void SelamVer() => Console.WriteLine("Selam");
+
+    }
+
+    class MusteriValidator
+    {
+        public static bool AdDogrula(string ad)
+        {
+            if (string.IsNullOrWhiteSpace(ad))
+                return false;
+            return true;
+        }
+    }
+}
