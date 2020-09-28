791954282971d6d668fe07aaef36ac437f5ac099
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-28 19:15:52 +0300

    Delegeler

---
+++ K03.Delegeler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace K03.Delegeler
+{
+    class Program
+    {
+        delegate void BenimDelegem(int sayi1, int sayi2);
+        static void Main(string[] args)
+        {
+            BenimDelegem delege = new BenimDelegem(Topla);
+            delege += Carp;
+            delege.Invoke(2, 3);
+            
+
+        }
+
+        static void Topla(int sayi1, int sayi2) => Console.WriteLine("$ Toplam: ", sayi1 + sayi2);
+        static void Carp(int sayi1, int sayi2) => Console.WriteLine("$ Carpma: ", sayi1*sayi2);
+    }
+}
