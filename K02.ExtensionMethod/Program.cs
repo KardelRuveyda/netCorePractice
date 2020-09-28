231fb9471f22b4c260b13c3bbf9ccebf5b2943b9
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-28 18:48:57 +0300

    Extension Metotlar

---
+++ K02.ExtensionMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace K02.ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatematikselHesaplama matematiksel = new MatematikselHesaplama();
+            int sonucCikarma= matematiksel.Cikar(5, 3);
+            int sonucCarpma = matematiksel.Carp(6, 2);
+
+            Console.WriteLine(sonucCarpma);
+            Console.WriteLine(sonucCikarma);
+           
+            Console.ReadLine();
+        }
+    }
+
+    class MatematikselHesaplama
+    {
+        public int Topla(int sayi1, int sayi2) => sayi1 + sayi2;
+        public int Bol(int sayi1, int sayi2) => sayi1 * sayi2;
+
+    }
+
+    static class MathExtension
+    {
+        public static int Cikar(this MatematikselHesaplama islem, int sayi1, int sayi2) => sayi1 - sayi2;
+        public static int Carp(this MatematikselHesaplama islem, int sayi1, int sayi2) => sayi1 * sayi2;
+    }
+}
